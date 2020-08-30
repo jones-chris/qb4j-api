65d79262781a88ec3377d23ef291bf7784067a1e
Chris Jones <chris.jones@cfacorp.com>
2020-08-30 06:06:40 -0400

    Parameterized cloudformation template and docker image name in deployment scripts

--- cicd/deployment/deployment_prod.sh
+++ cicd/deployment/deployment_prod.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# Find all the cloudformation templates.
-find ./cicd/cloudformation/stacks -name "*.cloudformation.yaml" > ./cf_stacks.txt
+## Find all the cloudformation templates.
+#find ./cicd/cloudformation/stacks -name "*.cloudformation.yaml" > ./cf_stacks.txt
+#
+#while read LINE; do
+#
+#  # Get file name.
+#  FILE_NAME=$(basename "$LINE")
+#
+#  # Get the stack name.
+#  NAME=$(echo "$FILE_NAME" | awk -F . '{print $2}')
+#
+#  echo "Deploying $FILE_NAME with stack name, $NAME"
+#
+#  # Deploy the cloudformation template.
+#  aws cloudformation deploy --template-file "$LINE" --stack-name "$NAME" --capabilities CAPABILITY_NAMED_IAM
+#
+#done < ./cf_stacks.txt
 
-while read LINE; do
+DOCKER_IMAGE_NAME=$1
 
-  # Get file name.
-  FILE_NAME=$(basename "$LINE")
-
-  # Get the stack name.
-  NAME=$(echo "$FILE_NAME" | awk -F . '{print $2}')
-
-  echo "Deploying $FILE_NAME with stack name, $NAME"
-
-  # Deploy the cloudformation template.
-  aws cloudformation deploy --template-file "$LINE" --stack-name "$NAME" --capabilities CAPABILITY_NAMED_IAM
-
-done < ./cf_stacks.txt
+aws cloudformation deploy \
+    --template-file ./cicd/cloudformation/stacks/qb4j-api-ecs-service.cloudformation.yaml \
+    --stack-name qb4j-api-ecs-service \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides DockerImage="$DOCKER_IMAGE_NAME"
