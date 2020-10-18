e05f42008f0f69080d326d8cc209ce332aba5b8f
Chris Jones <chris.jones@cfacorp.com>
2020-10-18 17:26:51 -0400

    Trying to not use HERE docs anymore

--- cicd/deployment/deployment_dev.sh
+++ cicd/deployment/deployment_dev.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # This script should log into the lightsail instance using the SSH private key, stop the qb4j-api docker container, pull down the new image, start the qb4j-api docker container.
+DOCKER_IMAGE_TAG=$1
+$QB4J_CONFIG=$2
+
 echo "Docker image tag / Project Version argument is: $1"
+echo "QB4J_CONFIG is $QB4J_CONFIG"
+
 
 # Get the private key, user name, and IP address for the lightsail instance to ssh into the lightsail instance.
 PRIVATE_KEY=$(aws ssm get-parameter --name /dev/qb4j_api_lightsail/ssh_key --with-decryption --output text --query Parameter.Value)
@@ -16,24 +21,18 @@ chmod 600 private_key.txt
 # ssh into the lightsail instance, pull the docker image, stop the existing docker container, start a container from the
 # new image, sleep for 10 seconds (so the container can start so that the prune command can accurately tell which images
 # are being used by containrs), and remove all unused images (so that the lightsail instance doesn't run out of disk space).
-ssh -i private_key.txt -tt -o StrictHostKeyChecking=no "$USER_NAME@$IP_ADDRESS" "/bin/bash -s $1 $2" << "EOF"
-  DOCKER_IMAGE_TAG=$1
-  QB4J_CONFIG=$2
-
-  echo "DOCKER_IMAGE_TAG is $DOCKER_IMAGE_TAG"
-  echo "$QB4J_CONFIG"
-
-  sudo docker pull joneschris/qb4j-api:"$DOCKER_IMAGE_TAG"
+ssh -i private_key.txt -tt -o StrictHostKeyChecking=no "$USER_NAME@$IP_ADDRESS" bash -c "'
+  sudo docker pull joneschris/qb4j-api:$DOCKER_IMAGE_TAG
 
   DOCKER_CONTAINER_ID_TO_STOP=$(sudo docker ps | grep 'qb4j-api' | awk '{ print $1 }')
 
-  sudo docker container stop "$DOCKER_CONTAINER_ID_TO_STOP"
+  sudo docker container stop $DOCKER_CONTAINER_ID_TO_STOP
 
-  sudo nohup docker container run --publish 8080:8080 --detach --restart always --env qb4j_config="$QB4J_CONFIG" joneschris/qb4j-api:"$DOCKER_IMAGE_TAG" &
+  sudo nohup docker container run --publish 8080:8080 --detach --restart always --env qb4j_config=$QB4J_CONFIG joneschris/qb4j-api:$DOCKER_IMAGE_TAG &
 
   sleep 10s
 
   sudo docker image prune -a --force
 
   exit
-EOF
+'"
