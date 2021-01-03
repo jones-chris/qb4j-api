e624d41635d7f3aeeeff68a77d934af54b4b7e59
jones-chris <christopheralan88@gmail.com>
2021-01-02 21:56:27 -0500

    Removed private_key.txt commands

--- cicd/deployment/swarm_deployment.sh
+++ cicd/deployment/swarm_deployment.sh
@@ -8,8 +8,8 @@ QB4J_CONFIG=$(cat ./cicd/deployment/swarm/swarm-qb4j.yml)
 echo "QB4J_CONFIG is $QB4J_CONFIG"
 
 # Put the private key in a txt file.
-echo "$AWS_LIGHTSAIL_SSH_KEY" > private_key.txt
-chmod 600 private_key.txt
+#echo "$AWS_LIGHTSAIL_SSH_KEY" > private_key.txt
+#chmod 600 private_key.txt
 
 # ssh into the lightsail instance, stop all containers, remove all containers, and remove all images.  This creates a
 # clean slate for the next command to pull the docker image and start a container with that docker image.
@@ -21,8 +21,8 @@ DOCKER_SWARM_YAML=$(cat ./cicd/deployment/swarm/docker-swarm.yml)
 echo "DOCKER_SWARM_YAML is: "
 echo "$DOCKER_SWARM_YAML"
 
-ssh -i private_key.txt -tt -o StrictHostKeyChecking=no "$AWS_LIGHTSAIL_USERNAME@$AWS_LIGHTSAIL_IP_ADDRESS" /bin/bash << "EOF"
-  export PROJECT_VERSION=$PROJECT_VERSIONV
+ssh -tt -o StrictHostKeyChecking=no "$AWS_LIGHTSAIL_USERNAME@dev.api.querybuilder4j.net" /bin/bash << "EOF"
+  export PROJECT_VERSION=$PROJECT_VERSION
   export UPDATE_CACHE=false
   export QB4J_CONFIG="$QB4J_CONFIG"
   "$DOCKER_SWARM_YAML" | sudo -E docker stack deploy --compose-file - qb4j
